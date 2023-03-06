ab5fd944fd22db593b1575e5c1419371e212c0b3
Jzakka <mouse4786@gmail.com>
2023-03-06 15:27:42 +0900

    목록기능구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,43 +1,51 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    private final Scanner sc;
+    private Scanner sc;
 
     public App(Scanner sc) {
         this.sc = sc;
     }
 
     public void run() {
-        System.out.println("== 명언 앱 ==");
+        System.out.println("== 명언 SSG ==");
 
-        long lastWiseSayingId = 0;
+        int wiseSayingsLastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
+        outer:
         while (true) {
             System.out.print("명령) ");
-
             String cmd = sc.nextLine().trim();
 
-            if (cmd.isEmpty()) continue;
-
             switch (cmd) {
-                case "종료":
-                    System.out.println("프로그램이 종료되었습니다.");
-                    return;
                 case "등록":
+                    int id = ++wiseSayingsLastId;
                     System.out.print("명언 : ");
-                    String content = sc.nextLine().trim();
+                    String content = sc.nextLine();
                     System.out.print("작가 : ");
-                    String authorName = sc.nextLine().trim();
+                    String author = sc.nextLine();
 
-                    long id = ++lastWiseSayingId;
+                    wiseSayings.add(new WiseSaying(id, content, author));
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
-                default:
-                    System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying = wiseSayings.get(i);
+
+                        System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.author, wiseSaying.content);
+                    }
                     break;
+                case "종료":
+                    break outer;
             }
         }
     }
--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -1,98 +1,101 @@
 package com.ll;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTests {
-    // 테스트유틸 테스트 시작
     @Test
-    @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
-    public void t1() {
+    public void 등록을_하면_명언과_작가를_물어본다() {
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                종료
+                """);
+
+        assertTrue(rs.contains("명언 : "));
+        assertTrue(rs.contains("작가 : "));
+    }
+
+    @Test
+    public void 등록을_하면_생성된_명언의_번호가_출력되어야_한다() {
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                등록
+                나에게 불가능이란 없다.
+                나폴레옹
+                종료
+                """);
+
+        assertTrue(rs.contains("1번 명언이 등록되었습니다."));
+        assertTrue(rs.contains("2번 명언이 등록되었습니다."));
+    }
+
+    @Test
+    public void 등록_후_목록에서_확인할_수_있어야_한다() {
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                등록
+                나에게 불가능이란 없다.
+                나폴레옹
+                목록
+                종료
+                """);
+
+        assertTrue(rs.contains("번호 / 작가 / 명언"));
+        assertTrue(rs.contains("----------------------"));
+        assertTrue(rs.contains("2 / 나폴레옹 / 나에게 불가능이란 없다."));
+        assertTrue(rs.contains("1 / 이순신 / 나의 죽음을 적들에게 알리지 말라"));
+    }
+
+    @Test
+    public void 프로그램_시작시_타이틀_출력_그리고_종료() {
+        String rs = AppTestRunner.run("""
+                종료
+                """);
+
+        assertTrue(rs.contains("== 명언 SSG =="));
+        assertTrue(rs.contains("명령)"));
+    }
+
+    @Test
+    public void 테스트() {
+        assertTrue(1 == 1);
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+    }
+
+    @Test
+    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
         Scanner sc = TestUtil.genScanner("안녕");
 
         String cmd = sc.nextLine().trim();
-        assertThat(cmd).isEqualTo("안녕");
+        assertEquals("안녕", cmd);
     }
 
     @Test
-    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
-    public void t2() {
-        // System.out 에 대한 화면출력 금지 시작
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+    public void 문자열을_파일에_저장() {
+        Util.file.mkdir("test_data");
+        Util.file.saveToFile("test_data/1.txt", "안녕");
 
-        System.out.print("안녕");
+        String body = Util.file.readFromFile("test_data/1.txt");
 
-        // 그 동안 출력되지 않던 내용들을 문자열로 반환
-        String rs = output.toString();
-
-        // System.out 에 대한 화면출력 금지 끝
-        TestUtil.clearSetOutToByteArray(output);
-
-        assertThat(rs).isEqualTo("안녕");
+        assertEquals("안녕", body);
     }
-
-    // 앱 테스트 시작
-    @Test
-    @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
-    public void t3() {
-        String rs = AppTestRunner.run("");
-
-        assertThat(rs)
-                .contains("== 명언 앱 ==")
-                .contains("명령) ")
-                .contains("프로그램이 종료되었습니다.")
-                .doesNotContain("올바르지 않은 명령입니다.");
-    }
-
-    @Test
-    @DisplayName("잘못된 명령어 입력에 대한 처리")
-    public void t4() {
-        String rs = AppTestRunner.run("종료2");
-
-        assertThat(rs)
-                .contains("올바르지 않은 명령입니다.");
-    }
-
-    @Test
-    @DisplayName("등록화면에서 명언과 작가를 입력받고, 명언을 생성한다.")
-    public void t5() {
-        String rs = AppTestRunner.run("""
-                등록
-                현재를 사랑하라.
-                작자미상
-                """);
-
-        assertThat(rs)
-                .contains("명언 : ")
-                .contains("작가 : ")
-                .contains("1번 명언이 등록되었습니다.");
-    }
-
-    @Test
-    @DisplayName("명언이 등록될 때 마다 생성되는 명언의 번호가 1씩 증가한다.")
-    public void t6() {
-        String rs = AppTestRunner.run("""
-                등록
-                현재를 사랑하라.
-                작자미상
-                등록
-                나의 죽음을 적들에게 알리지 마라.
-                이순신
-                등록
-                왼손은 거들뿐
-                강백호
-                """);
-
-        assertThat(rs)
-                .contains("1번 명언이 등록되었습니다.")
-                .contains("2번 명언이 등록되었습니다.")
-                .contains("3번 명언이 등록되었습니다.")
-                .doesNotContain("4번 명언이 등록되었습니다.");
-    }
-    // 앱 테스트 끝
 }
